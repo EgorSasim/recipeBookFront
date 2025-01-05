aa2f45489d7090f34a3b81d01e3eaad56857777e
Egor Sasim <egorsasim@mail.ru>
2025-01-05 13:37:58 +0300

    create empty vue app

---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+
+const openReadmeInEditor = () => fetch('/__open-in-editor?file=README.md')
+</script>
+
+<template>
+ <h1 @click="openReadmeInEditor">Hello world</h1>
+</template>
